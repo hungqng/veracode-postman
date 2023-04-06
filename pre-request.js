d2cfec60d97253e778e50e3708a99c94446aefaf
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2023-04-06 09:04:56 -0700

    Add files via upload

--- pre-request.js
+++ pre-request.js
@@ -1,11 +1,7 @@
-/*jshint esversion: 6 */
-
 var url = require('url');
 
-/* set Veracode API credentials in api_id and api_key in environment*/
-const id = pm.environment.get('api_id');
-const key = pm.environment.get('api_key');
-
+const id = pm.variables.get('id');
+const key = pm.variables.get('key');
 const authorizationScheme = 'VERACODE-HMAC-SHA-256';
 const requestVersion = "vcode_request_version_1";
 const nonceSize = 16;
@@ -13,39 +9,35 @@ const nonceSize = 16;
 function computeHashHex(message, key_hex) {
     return CryptoJS.HmacSHA256(message, CryptoJS.enc.Hex.parse(key_hex)).toString(CryptoJS.enc.Hex);
 }
-
-function calculateDataSignature(apikey, nonceBytes, dateStamp, data) {
-    let kNonce = computeHashHex(nonceBytes, apikey);
+function calulateDataSignature(key, nonceBytes, dateStamp, data) {
+    let kNonce = computeHashHex(nonceBytes, key);
     let kDate = computeHashHex(dateStamp, kNonce);
     let kSig = computeHashHex(requestVersion, kDate);
     return computeHashHex(data, kSig);
 }
-
 function newNonce() {
     return CryptoJS.lib.WordArray.random(nonceSize).toString().toUpperCase();
 }
-
 function toHexBinary(input) {
     return CryptoJS.enc.Hex.stringify(CryptoJS.enc.Utf8.parse(input));
 }
-
-function removePrefixFromApiCredential(input) {
-    return input.split('-').at(-1);
-}
-
 function calculateVeracodeAuthHeader(httpMethod, requestUrl) {
-    const formattedId = removePrefixFromApiCredential(id);
-    const formattedKey = removePrefixFromApiCredential(key);
-
-    let parsedUrl = url.parse(requestUrl);
-    let data = `id=${formattedId}&host=${parsedUrl.hostname}&url=${parsedUrl.path}&method=${httpMethod}`;
+    let urlExpanded = requestUrl;
+    while(urlExpanded.indexOf('{{') >= 0) {
+        let variableName = urlExpanded.substring(urlExpanded.indexOf('{{')+2, urlExpanded.indexOf('}}'));
+        let variableValue = pm.variables.get(variableName);
+        urlExpanded = urlExpanded.replace('{{'+variableName+'}}', variableValue);
+    }
+    let parsedUrl = url.parse(urlExpanded);
+    let data = `id=${id}&host=${parsedUrl.hostname}&url=${parsedUrl.path}&method=${httpMethod}`;
     let dateStamp = Date.now().toString();
-    let nonceBytes = newNonce();
-    let dataSignature = calculateDataSignature(formattedKey, nonceBytes, dateStamp, data);
-    let authorizationParam = `id=${formattedId},ts=${dateStamp},nonce=${toHexBinary(nonceBytes)},sig=${dataSignature}`;
-    return authorizationScheme + " " + authorizationParam;
+    let nonceBytes = newNonce(nonceSize);
+    let dataSignature = calulateDataSignature(key, nonceBytes, dateStamp, data);
+    let authorizationParam = `id=${id},ts=${dateStamp},nonce=${toHexBinary(nonceBytes)},sig=${dataSignature}`;
+    let header = authorizationScheme + " " + authorizationParam;
+    return header;
 }
-
-var {Property} = require('postman-collection');
-const substitutedUrl = Property.replaceSubstitutions(request.url, pm.variables.toObject());
-postman.setEnvironmentVariable('hmacAuthHeader', calculateVeracodeAuthHeader(request.method, substitutedUrl));
+pm.request.headers.add({
+    key: 'Authorization',
+    value: calculateVeracodeAuthHeader(request['method'], request['url'])
+});
